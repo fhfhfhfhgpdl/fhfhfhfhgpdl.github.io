ace4d86fd3103a7ad20337053b74ec7d2e631820
ParkEunSeo <peswitch6@gmail.com>
2022-07-28 23:28:24 +0900

    20220728

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -1,143 +1,127 @@
 AOS.init();
 
-//  Work experience cards
+// Work experience cards
 
-// const experiencecards = document.querySelector(".experience-cards");
-// const exp = [
-//   {
-//     title: "Software Development Intern",
-//     cardImage: "assets/images/experience-page/flipkart.jpg",
-//     place: "Flipkart",
-//     time: "(May, 2020 - present)",
-//     desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
-//   },
-//   {
-//     title: "Student Developer",
-//     cardImage: "assets/images/experience-page/gsoc.png",
-//     place: "Google Summer Of Code",
-//     time: "(Mar - Aug, 2019)",
-//     desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
-//   },
-//   {
-//     title: "Research Intern",
-//     cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-//     place: "IIT, Bombay",
-//     time: "(Dec, 19 - Jan, 20)",
-//     desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
-//   },
-// ];
-
-// const showCards2 = () => {
-//   let output = "";
-//   exp.forEach(
-//     ({ title, cardImage, place, time, desp }) =>
-//       (output += `        
-//     <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
-//       <div class="card card1">
-//         <img src="${cardImage}" class="featured-image"/>
-//         <article class="card-body">
-//           <header>
-//             <div class="title">
-//               <h3>${title}</h3>
-//             </div>
-//             <p class="meta">
-//               <span class="pre-heading">${place}</span><br>
-//               <span class="author">${time}</span>
-//             </p>
-//             <ol>
-//               ${desp}
-//             </ol>
-//           </header>
-//         </article>
-//       </div>
-//     </div>
-//       `)
-//   );
-//   experiencecards.innerHTML = output;
-// };
-// document.addEventListener("DOMContentLoaded", showCards2);
-
-// Volunteership Cards
-
-// const volunteership = document.querySelector(".volunteership");
-// const volunteershipcards = [
-//   {
-//     title: "GirlScript Summer Of Code 2020",
-//     cardImage: "assets/images/experience-page/1.jpg",
-//     description:
-//       "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
-//   },
-//   {
-//     title: "StudentCode-in 2020",
-//     cardImage: "assets/images/experience-page/2.jpg",
-//     description:
-//       "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
-//   },
-//   {
-//     title: "PClub Summer Of Code 2020",
-//     cardImage: "assets/images/experience-page/3.jpg",
-//     description:
-//       "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
-//   },
-//   {
-//     title: "Hakin-Codes",
-//     cardImage: "assets/images/experience-page/4.jpg",
-//     description:
-//       "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
-//   },
-// ];
-
-// const showCards = () => {
-//   let output = "";
-//   volunteershipcards.forEach(
-//     ({ title, cardImage, description }) =>
-//       (output += `        
-//       <div class="card volunteerCard" data-aos="fade-down" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="600" style="height: 550px;width:400px">
-      
-//       <img src="${cardImage}" height="250" width="65" class="card-img" style="border-radius:10px">
-//       <div class="content">
-//           <h2 class="volunteerTitle">${title}</h2><br>
-//           <p class="copy">${description}</p></div>
-      
-//       </div>
-//       `)
-//   );
-//   volunteership.innerHTML = output;
-// };
-// document.addEventListener("DOMContentLoaded", showCards);
-
-// Hackathon Section
-
-const genlink = document.querySelector(".hackathon-section");
-const pm = [
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
   {
     title: "Gen-Link",
-    subtitle: "Project Manager | Designer",
-    image: "assets/images/experience-page/gen-cha.png",
-    desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
+    cardImage: "assets/images/experience-page/gen-cha.png",
+    place: "Project Manager | Designer",
+    time: "2021 August ~ 2022 January",
+    desp: "<li>현대해상⨉어썸스쿨 주관 하이챌린지스쿨 프로젝트</li> <li>I was responsible for physical product and web page design, product management, planning and finance.</li> ",
+    goto: "genlink"
   },
   {
     title: "Eco-MRMR",
-    subtitle: "Project Manager | Product Designer | Designer | Developer",
-    image: "assets/images/experience-page/ulhacks.png",
-    desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
+    cardImage: "assets/images/experience-page/맵.png",
+    place: "Project Manager | Product Designer | Designer | Developer",
+    time: "2022 1st half",
+    desp: "<li>환경부 에코톤 출품 준비 프로젝트</li><li>I was in charge of project manager, web design, development and planning.</li>",
+    goto: "ecomrmr"
   },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp, goto }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <br/>
+            <ol>
+              ${desp}
+            </ol>
+            <div style='float:right'>
+            <a href="#${goto}" class="blog-slider__button">Read More</a> 
+            </div>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+// Hackathon Section
+
+const genlink = document.querySelector(".genlink-section");
+const pm = [
+  {
+    cla: "blog-slider__img",
+    title: "Gen-Link",
+    subtitle: "Project Manager | Designer",
+    image: 'img src = "assets/images/experience-page/gen-cha.png"',
+    desp: "세대를 연결하다, 젠링",
+    goto: "#", target:"_self", index:"go up"
+  },
+  {
+    cla: "blog-slider__img",
+    title: "📰News Letter",
+    subtitle: "",
+    image: 'img src = "assets/images/experience-page/genlink logo.jpg"',
+    desp: "수익금을 지역 청소년 상담 복지 센터에 기부해<br>다양한 인터넷 신문사에서 기사 업로드",
+    goto: "http://www.gynews.co.kr/news/articleView.html?idxno=13552",
+    target: "_blank", index:"Go to article"
+  },
+  {
+    cla:"blog-slider__item",
+    title: "💬Presentation",
+    subtitle: "",
+    image: 'iframe width="300" height="300" src="https://www.youtube.com/embed/FET2KHIWSCw?start=2748" title="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+    desp: "하이챌린지스쿨 연합 공유회 프로젝트 발표",
+    goto: "https://youtu.be/FET2KHIWSCw?t=2748",
+    target: "_blank", index:"Watch on youtube"
+  },
+  // {
+  //   cla:"",
+  //   title: "💬Presentation",
+  //   subtitle: "",
+  //   image: 'iframe width="300" height="300" src="https://www.youtube.com/embed/FET2KHIWSCw?t=2748" title="2021 현대해상X어썸스쿨 [하이챌린지스쿨 연합공유회]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/',
+  //   desp: "하이챌린지스쿨 연합 공유회 프로젝트 발표",
+  //   goto: "https://youtu.be/FET2KHIWSCw?t=2748",
+  //   target: "_blank", index:"Watch on youtube"
+  // },
+  // {
+  //   cla:"",
+  //   title: "💬Presentation",
+  //   subtitle: "",
+  //   image: 'iframe width="300" height="300" src="https://www.youtube.com/embed/FET2KHIWSCw?start=1668" title="2021 현대해상X어썸스쿨 [하이챌린지스쿨 연합공유회]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+  //   desp: "하이챌린지스쿨 연합 공유회 프로젝트 발표",
+  //   goto: "https://youtu.be/FET2KHIWSCw?t=2748",
+  //   target: "_blank", index:"Watch on youtube"
+  // },
 ];
 
 const showCards3 = () => {
   let output = "";
   pm.forEach(
-    ({ title, image, subtitle, desp }) =>
+    ({ title, image, subtitle, desp, goto, target, index, cla }) =>
       (output += `  
       <div class="blog-slider__item swiper-slide">
-        <div class="blog-slider__img">
-            <img src="${image}" alt="">
+        <div class="${cla}">
+        <${image}>
         </div>
         <div class="blog-slider__content">
           <div class="blog-slider__title">${title}</div>
           <span class="blog-slider__code">${subtitle}</span>
           <div class="blog-slider__text">${desp}</div>
-          <a href="#" class="blog-slider__button">Read More</a>   
+          <div style='float:right'>
+          <a href="${goto}" target="${target}" class="blog-slider__button">${index}</a> </div>
         </div>
       </div>
       `)
@@ -147,32 +131,28 @@ const showCards3 = () => {
 document.addEventListener("DOMContentLoaded", showCards3);
 
 
-const hackathonsection = document.querySelector(".hackathon-section");
-const mentor = [
+const ecomrmr = document.querySelector(".ecomrmr-section");
+const PM = [
   {
-    title: "ULHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/ulhacks.png",
+    title: "Gen-Link",
+    subtitle: "Project Manager | Designer",
+    image: "assets/images/experience-page/gen-cha.png",
+    desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
+    goto: "genlink"
+  },
+  {
+    title: "Eco-MRMR",
+    subtitle: "Project Manager | Product Designer | Designer | Developer",
+    image: "assets/images/experience-page/맵.png",
     desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
-  },
-  {
-    title: "WaffleHacks",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/wafflehacks.png",
-    desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
-  },
-  {
-    title: "Elevate Tech",
-    subtitle: "Judge",
-    image: "assets/images/experience-page/elevate.png",
-    desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
+    goto: "ecomrmr"
   },
 ];
 
 const showCards4 = () => {
   let output = "";
-  mentor.forEach(
-    ({ title, image, subtitle, desp }) =>
+  pm.forEach(
+    ({ title, image, subtitle, desp, goto }) =>
       (output += `  
       <div class="blog-slider__item swiper-slide">
         <div class="blog-slider__img">
@@ -182,11 +162,55 @@ const showCards4 = () => {
           <div class="blog-slider__title">${title}</div>
           <span class="blog-slider__code">${subtitle}</span>
           <div class="blog-slider__text">${desp}</div>
-          <a href="#" class="blog-slider__button">Read More</a>   
+          <a href="#${goto}" class="blog-slider__button">Read More</a>   
         </div>
       </div>
       `)
   );
-  hackathonsection.innerHTML = output;
+  ecomrmr.innerHTML = output;
 };
 document.addEventListener("DOMContentLoaded", showCards4);
+
+// const hackathonsection = document.querySelector(".hackathon-section");
+// const mentor = [
+//   {
+//     title: "ULHacks",
+//     subtitle: "Judge",
+//     image: "assets/images/experience-page/ulhacks.png",
+//     desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
+//   },
+//   {
+//     title: "WaffleHacks",
+//     subtitle: "Judge",
+//     image: "assets/images/experience-page/wafflehacks.png",
+//     desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
+//   },
+//   {
+//     title: "Elevate Tech",
+//     subtitle: "Judge",
+//     image: "assets/images/experience-page/elevate.png",
+//     desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
+//   },
+// ];
+
+// const showCards4 = () => {
+//   let output = "";
+//   mentor.forEach(
+//     ({ title, image, subtitle, desp }) =>
+//       (output += `  
+//       <div class="blog-slider__item swiper-slide">
+//         <div class="blog-slider__img">
+//             <img src="${image}" alt="">
+//         </div>
+//         <div class="blog-slider__content">
+//           <div class="blog-slider__title">${title}</div>
+//           <span class="blog-slider__code">${subtitle}</span>
+//           <div class="blog-slider__text">${desp}</div>
+//           <a href="#" class="blog-slider__button">Read More</a>   
+//         </div>
+//       </div>
+//       `)
+//   );
+//   hackathonsection.innerHTML = output;
+// };
+// document.addEventListener("DOMContentLoaded", showCards4);
